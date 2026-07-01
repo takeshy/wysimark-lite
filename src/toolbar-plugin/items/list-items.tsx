9058fc5a87f18ad6b5e93667824f73e92482594e
Takeshi Morita <takesy@joins.co.jp>
2026-07-01 13:17:22 +0900

    Show active list toolbar state

--- src/toolbar-plugin/items/list-items.tsx
+++ src/toolbar-plugin/items/list-items.tsx
@@ -1,8 +1,11 @@
 import { MenuItemData } from "../../shared-overlays"
+import { findElementUp } from "../../sink"
 
 import * as Icon from "../icons"
 import { t } from "../../utils/translations"
 import { listDepthItems } from "./block-items"
+
+const listItemTypes = ["unordered-list-item", "ordered-list-item", "task-list-item"]
 
 export const listItems: MenuItemData[] = [
   {
@@ -10,18 +13,21 @@ export const listItems: MenuItemData[] = [
     title: t("bulletList"),
     hotkey: "super+8",
     action: (editor) => editor.list.convertUnorderedList(true),
+    active: (editor) => !!findElementUp(editor, "unordered-list-item"),
   },
   {
     icon: Icon.ListNumbers,
     title: t("numberedList"),
     hotkey: "super+7",
     action: (editor) => editor.list.convertOrderedList(true),
+    active: (editor) => !!findElementUp(editor, "ordered-list-item"),
   },
   {
     icon: Icon.ListCheck,
     title: t("checkList"),
     hotkey: "super+9",
     action: (editor) => editor.list.convertTaskList(true),
+    active: (editor) => !!findElementUp(editor, "task-list-item"),
     show: (editor) => !editor.wysimark.disableTaskList,
   },
 ]
@@ -33,6 +39,7 @@ export const compactListItems: MenuItemData[] = [
     icon: Icon.ListNumbers,
     title: t("list"),
     more: true,
+    active: (editor) => !!findElementUp(editor, listItemTypes),
     children: [...listItems, "divider", ...listDepthItems],
   },
 ]
